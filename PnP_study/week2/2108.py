6fe3a2e8de6571178ef515dd9de1f2e12242aa07
HO <godqhr721@gmail.com>
2022-11-13 02:20:13 +0900

    modify find mode

--- PnP_study/week2/2108.py
+++ PnP_study/week2/2108.py
@@ -3,8 +3,12 @@ arr = []
 sum_val = 0
 check_arr = []
 mcheck_arr = []
-cnt_arr = []
-mcnt_arr = []
+many_arr = []
+a_cnt = 0
+b_cnt = 0
+cnt = 1
+
+
 
 max_cnt = 0
 cnt = 0
@@ -17,8 +21,8 @@ for _ in range(n):
     arr.append(int(input()))
 
 for _ in range(4001):
-    check_arr.append(0)
     mcheck_arr.append(0)
+    check_arr.append(0)
 
 for elem in arr:
     sum_val += elem
@@ -28,60 +32,44 @@ for elem in arr:
     else:
         check_arr[elem] += 1
 
-# 최빈값 구하기
-for i in range(4001):
-    if check_arr[i] > max_cnt:
-        max_cnt = check_arr[i]
-        cnt = check_arr[i]
-        cnt_arr = []
-        cnt_arr.append(i)
-    if mcheck_arr[i] > max_cnt:
-        max_cnt = mcheck_arr[i]
-        m_cnt  = mcheck_arr[i]
-        mcnt_arr = []
-        mcnt_arr.append(i)
-    elif check_arr[i] == max_cnt:
-            cnt_arr.append(i)
-    elif mcheck_arr[i] == max_cnt:
-            mcnt_arr.append(i)
+a_cnt = max(check_arr)
+b_cnt = max(mcheck_arr)
 
-if len(mcnt_arr) > 1:
-    mcnt_arr.reverse()
-# 중복 최빈값 잡기
-if m_cnt == cnt:
-    if len(mcnt_arr) != 0:
-        if len(mcnt_arr) < 2:
-            many_num = mcnt_arr[0] * -1
-        else:
-            many_num = mcnt_arr[1] * -1
-    else:
-        if len(cnt_arr) < 2:
-            many_num = cnt_arr[0]
-        else:
-            many_num = cnt_arr[1]
-elif m_cnt > cnt:
-    if len(mcnt_arr) == 1:
-        many_num = mcnt_arr[0] * -1
-    else:
-        many_num = mcnt_arr[1] * -1
+if a_cnt == b_cnt:
+    max_cnt = a_cnt
 else:
-    if len(cnt_arr) == 1:
-        many_num = cnt_arr[0]
-    else:
-        many_num = cnt_arr[1]
-
-
-
+    max_cnt = max(a_cnt,b_cnt)
 
 arr.sort()
+
+for i in range(n-1):
+    if arr[i] == arr[i+1]:
+        cnt += 1
+
+    else:
+        cnt = 1
+
+    if max_cnt == cnt:
+        many_arr.append(arr[i])
+
+if len(arr) == 1:
+    b = arr[0]
+
+elif len(many_arr) < 2:
+    b = many_arr[0]
+else:
+    b = many_arr[1]
+
+
 if sum_val < 0:
     a = int(sum_val/5 - 0.5)
 else:
     a = int(sum_val/n + 0.5)
 print(a)
 print(arr[n//2])
-print(many_num)
+print(b)
 print(arr[n-1] - arr[0])
+
 
 
 
