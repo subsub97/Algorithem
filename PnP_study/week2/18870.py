6aac988d750b99b725bae86e804e6176d43531ee
HO <godqhr721@gmail.com>
2022-11-13 21:02:32 +0900

    using set

--- PnP_study/week2/18870.py
+++ PnP_study/week2/18870.py
@@ -2,26 +2,19 @@
 n = int(input())
 
 arr = list(map(int,input().split()))
-sorted_arr = sorted(arr)
+sorted_arr = sorted(set(arr))
 check_dict = {}
 cnt = 0
 
-for i in range(n-1):
-    if sorted_arr[i] == sorted_arr[i+1]:
-        pass
+for i in range(len(sorted_arr)):
+    if i ==0:
+        check_dict={sorted_arr[i]:cnt}
+        cnt += 1
     else:
-        if len(check_dict) == 0:
-            check_dict = {sorted_arr[i]:cnt}
-            cnt += 1
-        else:
-            check_dict[sorted_arr[i]] = cnt
-            cnt += 1
-    if i == n-2:
-        if sorted_arr[i] == sorted_arr[i+1]:
-            check_dict[sorted_arr[i]] = cnt
-        else:
-            check_dict[sorted_arr[i]] = cnt
-            check_dict[sorted_arr[i+1]] = cnt + 1
+        check_dict[sorted_arr[i]] = cnt
+        cnt +=1
+
+
 
 
 
