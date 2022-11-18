dfab04305f9d48e354fd4a41e6ee18a614a25a29
HO <godqhr721@gmail.com>
2022-11-18 21:57:43 +0900

    can't find correct answer

--- PnP_study/week2/1629.py
+++ PnP_study/week2/1629.py
@@ -1,28 +1,36 @@
 # 분할정복을 이용해서 문제 풀어보기
-
+A,B,C = map(int,input().split())
 def find_mode(A,num,check_point):
     if check_point ==0:
         check_point = A
+
     if num % 2 != 0:
         num = num // 2
         if num == 1:
-            A = (A * A) * check_point
+            A = A % C
             return A, check_point
         A,check_point = find_mode(A,num,check_point)
-        A = (A*A)*check_point
+        A = (A * A) % C
+        A = (A * A*(check_point % C)) % C
         return A, check_point
 
     else:
         num = num // 2
 
     if num == 1:
-        A *= A
+        A = A % C
         return A , check_point
-    A,check_point = find_mode(A,num,check_point)
-    A *= A
+    elif num == 3:
+        A, check_point = find_mode(A, num, check_point)
+        A = (A * A) % C
+        A = (A * A * (check_point % C)) % C
+        return A, check_point
+    else:
+        A,check_point = find_mode(A,num,check_point)
+        A = (A * A) % C
     return A ,check_point
 
-#A,B,C = map(int,input().split())
-result,check_point = find_mode(2,100,0)
 
-print(result % 3)
+result,check_point = find_mode(A,B,0)
+
+print(result)
