821162ff6bf731adc00ce259008ff94276392bc2
HO <godqhr721@gmail.com>
2022-11-19 10:57:48 +0900

    try method -1  recursive

--- PnP_study/week2/1629.py
+++ PnP_study/week2/1629.py
@@ -1,36 +1,16 @@
 # 분할정복을 이용해서 문제 풀어보기
 A,B,C = map(int,input().split())
-def find_mode(A,num,check_point):
-    if check_point ==0:
-        check_point = A
-
-    if num % 2 != 0:
-        num = num // 2
-        if num == 1:
-            A = A % C
-            return A, check_point
-        A,check_point = find_mode(A,num,check_point)
-        A = (A * A) % C
-        A = (A * A*(check_point % C)) % C
-        return A, check_point
-
-    else:
-        num = num // 2
-
+# 나누기 분배법칙 (A X B) % C = ((A % C) X (A % C)) % C
+def find_mode(A,num):
     if num == 1:
-        A = A % C
-        return A , check_point
-    elif num == 3:
-        A, check_point = find_mode(A, num, check_point)
-        A = (A * A) % C
-        A = (A * A * (check_point % C)) % C
-        return A, check_point
+        return A % C
     else:
-        A,check_point = find_mode(A,num,check_point)
-        A = (A * A) % C
-    return A ,check_point
+        sort_num = find_mode(A,num // 2)
+        if num % 2 != 0: # 홀수인 경우
+            return (sort_num * sort_num * A) % C
+        else: # 짝수인 경우
+            return (sort_num * sort_num) % C
+print(find_mode(A,B))
 
 
-result,check_point = find_mode(A,B,0)
 
-print(result)
