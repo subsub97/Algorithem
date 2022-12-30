46364f34e9ce9ab1d1eb911e0d15e8e300e4d3e0
HO <godqhr721@gmail.com>
2022-12-30 18:33:54 +0900

    solved

---
+++ Study/Greedy/11047.py
@@ -0,0 +1,25 @@
+# Greedy try1
+
+n,k = map(int,input().split())
+
+# 각 동전의 크기별로 정리된 배열 생성
+coin_arr = [
+    int(input())
+    for _ in range(n)
+]
+cnt =0
+num = -1 #파이썬 slicing을 이용하기위한 변수
+
+while True:
+    if k - coin_arr[num] >= 0:
+        k -= coin_arr[num]
+        cnt += 1
+    else:
+        num -= 1
+        if k == 0:
+            break
+print(cnt)
+
+
+
+
