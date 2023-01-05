5f96047dfc1c2af5c9a490dcd4be18cfb69edeac
HO <godqhr721@gmail.com>
2023-01-05 20:31:43 +0900

    fail timeout

--- Study/DP/1912.py
+++ Study/DP/1912.py
@@ -1,7 +1,14 @@
-#연속합 DP try2
+#연속합 DP try5
 '''
 try 2 try1에서는 2차원 배열을 사용했지만 이번에는 1차원 배열로 해결해보기
+try 3 try2에서는 채울수있는 경우에는 모든 값을 num_arr에 추가 했지만
+      이전 num_arr[j-1]값이 음수라면 최대가 될 수 없기에 음수가 나온다면
+      바로 다음 값을 계산하도록 수정해보았다.
+try 4 input()을 sys.stdin.readline()으로 바꿔보았다.
 '''
+import sys
+
+
 n = int(input())
 
 #문제에서 주어진 최저 값이 -1000 이기에 max를 그보다 작은 수로 설정한다면
@@ -10,7 +17,7 @@ max = -1001
 
 # tuple로 한 이유는 입력받은 값이 변동될 일이 없기에 tuple로 했다.
 #(솔직히 상관없음)
-num_arr = tuple(map(int,input().split()))
+num_arr = tuple(map(int,sys.stdin.readline().split()))
 
 # 값을 기억 저장할 DP 리스트 생성
 memo = [
@@ -24,9 +31,13 @@ for i in range(n):
             if max < memo[i]:
                 max = memo[i]
         else:
+
             memo[j] = memo[j-1] + num_arr[j]
+            if memo[j] < 0:
+                break
             if max < memo[j]:
                 max = memo[j]
+
 print(max)
 
 
