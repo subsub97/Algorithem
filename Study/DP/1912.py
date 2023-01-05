9557d20d5895f72358b3f05382914c39cec537a7
HO <godqhr721@gmail.com>
2023-01-05 21:11:21 +0900

    fail wrong answer

--- Study/DP/1912.py
+++ Study/DP/1912.py
@@ -5,6 +5,9 @@ try 3 try2에서는 채울수있는 경우에는 모든 값을 num_arr에 추가
       이전 num_arr[j-1]값이 음수라면 최대가 될 수 없기에 음수가 나온다면
       바로 다음 값을 계산하도록 수정해보았다.
 try 4 input()을 sys.stdin.readline()으로 바꿔보았다.
+try 5 이전 num_arr[j-1] 값이 음수였을때 break를 했었지만 이번엔
+      그 break 포인트 이후부터 num_arr 값을 넣어주는 방식으로 접근해보았다.
+      max 대상자는 음수값 이후의 핪에서만 나오는 것을 이용해봤다.
 '''
 import sys
 
@@ -23,20 +26,28 @@ num_arr = tuple(map(int,sys.stdin.readline().split()))
 memo = [
     0 for _ in range(n)
 ]
+cnt = 0
+while cnt < n:
+    memo[cnt] = num_arr[cnt]
+    if cnt == 0:
+        if memo[cnt] > max:
+            max = memo[cnt]
+        cnt += 1
+    for i in range(cnt,n):
+        if num_arr[cnt] > max:
+            max = num_arr[cnt]
 
-for i in range(n):
-    for j in range(i,n):
-        if j == i:
-            memo[j] = num_arr[j]
-            if max < memo[i]:
-                max = memo[i]
-        else:
+        if memo[i - 1] + num_arr[i] < 0:
+            cnt = i
+            break
 
-            memo[j] = memo[j-1] + num_arr[j]
-            if memo[j] < 0:
-                break
-            if max < memo[j]:
-                max = memo[j]
+        memo[i] = memo[i - 1] + num_arr[i]
+
+        if memo[i-1] + num_arr[i] > max:
+            max = memo[i-1] + num_arr[i]
+        if i == n-1:
+            cnt = i
+    cnt +=1
 
 print(max)
 
