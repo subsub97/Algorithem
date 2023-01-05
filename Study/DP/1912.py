ae9812b1893de3087c320e7944ce126fedf54833
HO <godqhr721@gmail.com>
2023-01-05 20:17:42 +0900

    fail timeout

--- Study/DP/1912.py
+++ Study/DP/1912.py
@@ -1,5 +1,7 @@
-#연속합 DP try1
-
+#연속합 DP try2
+'''
+try 2 try1에서는 2차원 배열을 사용했지만 이번에는 1차원 배열로 해결해보기
+'''
 n = int(input())
 
 #문제에서 주어진 최저 값이 -1000 이기에 max를 그보다 작은 수로 설정한다면
@@ -12,20 +14,19 @@ num_arr = tuple(map(int,input().split()))
 
 # 값을 기억 저장할 DP 리스트 생성
 memo = [
-    [0]*n for _ in range(n)
+    0 for _ in range(n)
 ]
 
 for i in range(n):
     for j in range(i,n):
-        if j == 0:
-            memo[i][j] = num_arr[i]
-            if max < memo[i][j]:
-                max = memo[i][j]
+        if j == i:
+            memo[j] = num_arr[j]
+            if max < memo[i]:
+                max = memo[i]
         else:
-            memo[i][j] = memo[i][j-1] + num_arr[j]
-            if max < memo[i][j]:
-                max = memo[i][j]
-
+            memo[j] = memo[j-1] + num_arr[j]
+            if max < memo[j]:
+                max = memo[j]
 print(max)
 
 
