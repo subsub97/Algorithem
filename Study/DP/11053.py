2138e6a5d54d72be9450e01ebc227c9d6537c05f
HO <godqhr721@gmail.com>
2023-01-06 18:35:21 +0900

    try 2 fail

--- Study/DP/11053.py
+++ Study/DP/11053.py
@@ -1,6 +1,9 @@
-# 가장 긴 증가하는 부분 수열 DP try1
+# 가장 긴 증가하는 부분 수열 DP try2
 '''
 try1 틀렸습니다.
+try2 current_num을 바꾸는 조건을 바꾸었다.
+     이전에는 j가 증가할떄 마다 바꾸었는데 이러한 경우에
+     중복된 값을 count하는 경우가 발생한다.
 '''
 import sys
 n = int(input())
@@ -20,7 +23,7 @@ for i in range(n):
     for j in range(i+1,n):
          if current_num < num_arr[j]:
              memo[i] += 1
-         current_num = num_arr[j]
+             current_num = num_arr[j]
     if max_cnt < memo[i]:
         max_cnt = memo[i]
 
