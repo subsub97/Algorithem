3b172de07353b6873409f3e505f491f1df443a42
HO <godqhr721@gmail.com>
2023-01-11 22:43:01 +0900

    upload

--- Study/test1.py
+++ Study/test1.py
@@ -1,16 +1,7 @@
-import sys
+n= int(input())
 
-student_arr = []
-n = int(input())
+memo = [
+    [i] * i for i in range(1,n+1)
+]
 
-for i in range(n):
-    student_arr = list(map(int,sys.stdin.readline().split()))
-    sum_val = sum(student_arr)
-    sum_val -= student_arr[0]
-    average = sum_val/student_arr[0]
-    cnt = 0
-    for j in range(1,student_arr[0]+1):
-        if student_arr[j] >  average:
-            cnt += 1
-
-    print(f"{(cnt/student_arr[0])*100:.3f}%")
+print(memo)
