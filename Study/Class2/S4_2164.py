e10a4d14f42e62b52ceb8c939a2a7404e8627814
HO <godqhr721@gmail.com>
2023-01-19 15:39:47 +0900

    solved using deque library

--- Study/Class2/S4_2164.py
+++ Study/Class2/S4_2164.py
@@ -1,31 +1,26 @@
+from collections import deque
 import sys
 
 n = int(sys.stdin.readline().strip())
 
-card_list = [
-    i for i in range(1,n+1)
-]
+card_queue = deque()
 
-def Qpop(list):
-    for i in range(len(list)):
-        if i == 0:
-            pop_num = list[i]
-        if i+1 < len(list):
-            list[i] = list[i+1]
-    if len(list) >= 2:
-        list.pop()
-    return pop_num
+for i in range(1,n+1):
+    card_queue.append(i)
 
 cnt = 0
-while len(card_list) != 1:
+while len(card_queue) != 1:
     cnt += 1
-    #짝수번째 인경우 pop한 수를 맨뒤로 붙여야하기때문
-    if cnt % 2 == 0:
-        num = Qpop(card_list)
-        card_list.append(num)
+    if cnt % 2== 0:
+        num = card_queue.popleft()
+        card_queue.append(num)
     else:
-        num = Qpop(card_list)
+        card_queue.popleft()
 
-print(card_list[0])
+print(card_queue[0])
+
+
+
+
 
 
