5e39a87bc049de9e856368093e4814db9954ffeb
HO <godqhr721@gmail.com>
2023-01-27 18:30:15 +0900

    solved myself but need to modify code

--- Study/Class3/S2_1541.py
+++ Study/Class3/S2_1541.py
@@ -4,51 +4,37 @@
 # 1. 음수가 나오면 항상 그 뒤에있는 모든 덧셈까지는 모아서 괄호에 묶자
 # 수식을 받자
 op_line = input()
-sum_val = 0
 min_val = 0
+sum_val = 0
 flag = 0
-# 두개의 연산자가 모두 존재하는 경우
-if '+'  in (op_line):
-    if '-' in(op_line):
-        split_Mnum = op_line.split('-') # - 를 기준으로 나누자
-        for num in split_Mnum:
-            if '+' in num:
-               split_Pnum = num.split('+')
-               for val in split_Pnum:
-                   sum_val += int(val)
-               sum_val = -sum_val
-               min_val += sum_val
+num = ''
 
-            else:
-                if num == '':
-                    flag = 1
-                    min_val -= sum_val
-                else:
-                    sum_val = int(num)
-                    if flag == 1:
-                        min_val -= sum_val
-
-                    else:
-                        flag = 1
-                        min_val += sum_val
-            sum_val =0
-    else: # + 연산자로만 이루어진 수식
-        split_Pnum = op_line.split('+')
-        for num in split_Pnum:
-            sum_val += int(num)
-        min_val += sum_val
-else: # -로 만 연산자가 이루어진 경우
-    split_Mnum = op_line.split('-')
-    for num in split_Mnum:
-        if num == '':
+for i in range(len(op_line)):
+    cur_char = op_line[i]
+    if cur_char == '-' or cur_char == '+':
+        if flag == 0 and cur_char == '-':
             flag = 1
+            if i > 0:
+                min_val += int(num)
+            num =''
             continue
-        if flag == 1:
-            sum_val -= int(num)
-        if flag == 0:
-            flag = 1
-            sum_val += int(num)
-    min_val = sum_val
 
+
+        if flag == 0 and i > 0:
+            min_val += int(num)
+
+        else:
+            if flag == 1:
+                min_val -= int(num)
+            else:
+                min_val += int(num)
+        num = ''
+    else:
+        num = num + cur_char
+
+if flag == 1:
+    min_val -= int(num)
+else:
+    min_val += int(num)
 print(min_val)
 
