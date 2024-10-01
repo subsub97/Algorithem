4302513405f0a9ade8d197b177f588c2126a5891
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-10-01 15:12:42 +0900

    [Gold V] Title: 벽장문의 이동, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
@@ -7,6 +7,7 @@ public class Main {
     static int N;
     static int K;
     static int[] cmd;
+    static int[][][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,6 +22,16 @@ public class Main {
         K = Integer.parseInt(br.readLine().trim());
 
         cmd = new int[K];
+        dp = new int[K][N + 1][N + 1];
+
+        for (int i = 0; i < K; i++) {
+            for (int j = 0; j <= N; j++) {
+                for (int k = 0; k <= N; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+
         for (int i = 0; i < K; i++) {
             cmd[i] = Integer.parseInt(br.readLine().trim());
         }
@@ -34,9 +45,15 @@ public class Main {
         }
         int destination = cmd[depth];
 
+        if (dp[depth][doorA][doorB] != -1) {
+            return dp[depth][doorA][doorB];
+        }
+
         int doorACost = Math.abs(destination - doorA) + calcMinCost(depth + 1, destination, doorB);
         int doorBCost = Math.abs(destination - doorB) + calcMinCost(depth + 1, doorA, destination);
 
-        return Math.min(doorACost, doorBCost);
+        dp[depth][doorA][doorB] = Math.min(doorACost, doorBCost);
+
+        return dp[depth][doorA][doorB];
     }
 }
