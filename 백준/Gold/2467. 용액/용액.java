55e6792c2eec046107daa90566705571cbdd02be
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2024-10-31 23:26:13 +0900

    [Gold V] Title: 용액, Time: 348 ms, Memory: 28752 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -2,42 +2,53 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int N;
+    static int[] liquid;
+    static long ans = Long.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] liquid = new int[N];
+        N = Integer.parseInt(br.readLine());
+        liquid = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         for(int i = 0; i < N; i++) {
             liquid[i] = Integer.parseInt(st.nextToken());
         }
-        
+
+
+
         int[] answer = new int[2];
-        long minSum = Long.MAX_VALUE;
-        
-        // 왼쪽 용액을 기준으로 반복
-        for(int i = 0; i < N-1; i++) {
-            int left = i + 1;
-            int right = N - 1;
-            
-            while(left <= right) {
-                int mid = (left + right) / 2;
-                long sum = (long)liquid[i] + liquid[mid];
-                
-                if(Math.abs(sum) < minSum) {
-                    minSum = Math.abs(sum);
+        answer[0] = liquid[0];
+        answer[1] = liquid[N-1];
+
+        for(int i = 0; i < N - 1; i++) {
+            //여기서 left는 고정하고 이분탐색으로 짝꿍 찾기
+            int s = i + 1;
+            int e = N-1;
+
+            while(s <= e) {
+                int mid = (s + e) / 2;
+                long curSum = (long) liquid[i] + liquid[mid];
+
+                if(Math.abs(curSum) < ans) {
                     answer[0] = liquid[i];
                     answer[1] = liquid[mid];
+                    ans = Math.abs(curSum);
                 }
-                
-                if(sum < 0) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+
+                if(curSum > 0) {
+                    e = mid - 1;
+                }
+                else {
+                    s = mid + 1;
+                }
                 }
             }
-        }
-        
+
         System.out.println(answer[0] + " " + answer[1]);
     }
+
 }
