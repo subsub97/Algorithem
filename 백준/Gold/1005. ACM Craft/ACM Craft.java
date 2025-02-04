19bf34c64ffefd19a832d7e213cc7b856faf417e
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2025-02-05 00:45:46 +0900

    [Gold III] Title: ACM Craft, Time: 1396 ms, Memory: 221496 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -12,6 +12,7 @@ public class Main {
     static int[] costs;
     static int ans;
     static ArrayList<ArrayList<Integer>> edges;
+    static ArrayList<ArrayList<Integer>> redges;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,12 +30,15 @@ public class Main {
             inDegree = new int[N + 1];
             dp = new int[N + 1][N + 1];
             edges = new ArrayList<>();
+            redges = new ArrayList<>();
             costs = new int[N + 1];
+            redges.add(new ArrayList<>());
             edges.add(new ArrayList<>());
 
             for (int i = 1; i <= N; i++) {
                 costs[i] = Integer.parseInt(st.nextToken());
                 edges.add(new ArrayList<>());
+                redges.add(new ArrayList<>());
             }
 
             for (int i = 0; i < K; i++) {
@@ -43,6 +47,7 @@ public class Main {
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
                 edges.get(from).add(to);
+                redges.get(to).add(from);
                 inDegree[to]++;
             }
 
@@ -58,14 +63,15 @@ public class Main {
 
             while(!q.isEmpty()) {
                 int cur = q.poll();
+                redges.get(cur).add(0);
                 ans = Math.max(ans, costs[cur]);
                 for(int i = 0; i < edges.get(cur).size(); i++) {
                     //연결된 간선으로 이동
                     int from = edges.get(cur).get(i);
                     inDegree[from]--;
                     // cur -> from 비용
-                    for(int pre = 0; pre < N + 1; pre++) {
-                        dp[cur][from] = Math.max(dp[cur][from], dp[pre][cur] + costs[cur]);
+                    for(int j = 0; j < redges.get(cur).size(); j++) {
+                        dp[cur][from] = Math.max(dp[cur][from], dp[redges.get(cur).get(j)][cur] + costs[cur]);
                     }
 
                     if(from == target) {
@@ -77,6 +83,7 @@ public class Main {
                         if(from == target) {
                             ans = Math.max(ans,dp[cur][from] + costs[from]);
                             q = new LinkedList<>();
+                            break;
                         }
                     }
                 }
