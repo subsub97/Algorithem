50e38de158d47ea6d3b267131c563bfabe0d2e87
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2025-02-05 00:48:11 +0900

    [Gold III] Title: ACM Craft, Time: 700 ms, Memory: 239648 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -6,93 +6,79 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int T,N,K;
+    static int T, N, K;
     static int[] inDegree;
-    static int[][] dp;
-    static int[] costs;
-    static int ans;
-    static ArrayList<ArrayList<Integer>> edges;
-    static ArrayList<ArrayList<Integer>> redges;
+    static int[] dp;        // 각 건물까지 걸리는 최대 시간
+    static int[] costs;     // 각 건물의 건설 시간
+    static ArrayList<ArrayList<Integer>> graph;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int t = 0; t < T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
-            st = new StringTokenizer(br.readLine());
-
+            // 초기화
             inDegree = new int[N + 1];
-            dp = new int[N + 1][N + 1];
-            edges = new ArrayList<>();
-            redges = new ArrayList<>();
+            dp = new int[N + 1];
             costs = new int[N + 1];
-            redges.add(new ArrayList<>());
-            edges.add(new ArrayList<>());
+            graph = new ArrayList<>();
 
-            for (int i = 1; i <= N; i++) {
-                costs[i] = Integer.parseInt(st.nextToken());
-                edges.add(new ArrayList<>());
-                redges.add(new ArrayList<>());
+            for (int i = 0; i <= N; i++) {
+                graph.add(new ArrayList<>());
             }
 
+            // 건설 시간 입력
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                costs[i] = Integer.parseInt(st.nextToken());
+            }
+
+            // 건설 순서 입력
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-
                 int from = Integer.parseInt(st.nextToken());
                 int to = Integer.parseInt(st.nextToken());
-                edges.get(from).add(to);
-                redges.get(to).add(from);
+                graph.get(from).add(to);
                 inDegree[to]++;
             }
 
             int target = Integer.parseInt(br.readLine());
-            ans = 0;
-            Queue<Integer> q = new LinkedList<>();
-
-            for(int i = 1; i <= N; i++) {
-                if(inDegree[i] == 0) {
-                    q.add(i);
-                }
-            }
-
-            while(!q.isEmpty()) {
-                int cur = q.poll();
-                redges.get(cur).add(0);
-                ans = Math.max(ans, costs[cur]);
-                for(int i = 0; i < edges.get(cur).size(); i++) {
-                    //연결된 간선으로 이동
-                    int from = edges.get(cur).get(i);
-                    inDegree[from]--;
-                    // cur -> from 비용
-                    for(int j = 0; j < redges.get(cur).size(); j++) {
-                        dp[cur][from] = Math.max(dp[cur][from], dp[redges.get(cur).get(j)][cur] + costs[cur]);
-                    }
-
-                    if(from == target) {
-                        ans = Math.max(ans,dp[cur][from] + costs[from]);
-                    }
-
-                    if(inDegree[from] == 0) {
-                        q.add(from);
-                        if(from == target) {
-                            ans = Math.max(ans,dp[cur][from] + costs[from]);
-                            q = new LinkedList<>();
-                            break;
-                        }
-                    }
-                }
-            }
-            sb.append(ans).append("\n");
+            sb.append(topologicalSort(target)).append('\n');
         }
         System.out.print(sb);
     }
 
+    static int topologicalSort(int target) {
+        Queue<Integer> q = new LinkedList<>();
 
+        // 시작점 찾기
+        for (int i = 1; i <= N; i++) {
+            if (inDegree[i] == 0) {
+                q.add(i);
+                dp[i] = costs[i];
+            }
+        }
 
+        // 위상 정렬
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            if (cur == target) break;
+
+            for (int next : graph.get(cur)) {
+                // 다음 건물까지의 최대 시간 갱신
+                dp[next] = Math.max(dp[next], dp[cur] + costs[next]);
+                if (--inDegree[next] == 0) {
+                    q.add(next);
+                }
+            }
+        }
+
+        return dp[target];
+    }
 }
