ee21ea1b9d6f9a2c655fcac20fbe45cd32bc868f
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2025-02-06 17:45:52 +0900

    [Gold I] Title: 리그 오브 레전설 (Large), Time: 2152 ms, Memory: 30628 KB -BaekjoonHub

--- 백준/Gold/17272. 리그 오브 레전설 （Large）/리그 오브 레전설 （Large）.java
+++ 백준/Gold/17272. 리그 오브 레전설 （Large）/리그 오브 레전설 （Large）.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 public class Main {
     static long N;
     static int M;
-    static long[] dp;
     static long MOD = 1_000_000_007;
 
     public static void main(String[] args) throws IOException {
@@ -15,8 +14,11 @@ public class Main {
 
         N = Long.parseLong(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        
-        long[][] C = createMatrix();
+
+        long[][] C = new long[M][M];
+
+        C[0][0] = C[0][M-1] = 1;
+        for (int k = 0; k < M - 1; k++) C[1 + k][k] = 1;
         C = matrixCalc(C, N);
 
         System.out.println(C[0][0]);
@@ -26,7 +28,6 @@ public class Main {
             return A;
         }
 
-        // 현재 행렬 상태 저장
         long[][] temp = new long[M][M];
         for(int i = 0; i < M; i++) {
             for(int j = 0; j < M; j++) {
@@ -34,10 +35,8 @@ public class Main {
             }
         }
 
-        // n/2 번 거듭제곱 계산
         long[][] mulA = matrixCalc(A, n/2);
 
-        // 행렬 곱셈 계산 (mulA * mulA)
         long[][] result = new long[M][M];
         for(int i = 0; i < M; i++) {
             for(int j = 0; j < M; j++) {
@@ -49,7 +48,6 @@ public class Main {
             }
         }
 
-        // n이 홀수일 경우 한 번 더 곱하기
         if((n % 2) == 1) {
             long[][] finalResult = new long[M][M];
             for(int i = 0; i < M; i++) {
@@ -65,58 +63,6 @@ public class Main {
         }
 
         return result;
-    }
-
-//    private static long[][] metrixCalc(long[][] A, long n) {
-//        if(n <= 1) {
-//            return A;
-//        }
-//
-//        long tA11 = A[0][0];
-//        long tA12 = A[0][1];
-//        long tA21 = A[1][0];
-//        long tA22 = A[1][1];
-//
-//        long[][] mulA = metrixCalc(A, n /2);
-//
-//        long a11 = (A[0][0] * A[0][0] + A[0][1] * A[1][0]) % MOD;
-//        long a12 = (A[0][0] * A[0][1] + A[0][1] * A[1][1]) % MOD;
-//        long b11 = (A[1][0] * A[0][0] + A[1][1] * A[1][0]) % MOD;
-//        long b12 = (A[1][0] * A[0][1] + A[1][1] * A[1][1]) % MOD;
-//
-//        mulA[0][0] = a11;
-//        mulA[0][1] = a12;
-//        mulA[1][0] = b11;
-//        mulA[1][1] = b12;
-//
-//        if(((n % 2) ==0)) {
-//            return mulA;
-//        }
-//        else{
-//            a11 = mulA[0][0] * tA11 + mulA[0][1] * tA12;
-//            a12 = mulA[0][0] * tA12 + mulA[0][1] * tA22;
-//            b11 = mulA[1][0] * tA11 + mulA[1][1] * tA21;
-//            b12 = mulA[1][0] * tA12 + mulA[1][1] * tA22;
-//
-//            mulA[0][0] = a11 % MOD;
-//            mulA[0][1] = a12 % MOD;
-//            mulA[1][0] = b11 % MOD;
-//            mulA[1][1] = b12 % MOD;
-//            return mulA;
-//        }
-//    }
-
-    public static long[][] createMatrix() {
-        long[][] matrix = new long[M][M];
-
-        matrix[0][0] = 1;
-        matrix[0][M-1] = 1;
-
-        for(int i = 1; i < M; i++) {
-            matrix[i][i-1] = 1;
-        }
-
-        return matrix;
     }
 }
 
